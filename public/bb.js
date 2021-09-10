0184301665f39346cdfc21a6038dc4780a2bb135
fuyx <fuyx@zhong-ying.com>
2021-09-10 17:14:03 +0800

    copy 静态资源

--- public/bb.js
+++
@@ -1,1 +0,0 @@
-console.log(909044)
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,27 @@
 const path = require('path');
 const proxy = require('./proxy.config').proxy;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    target:'web',
+    target: 'web',
     entry: {
         index: './index.js',
         aa: './aa.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+        filename: 'js/[name].bundle.js',
         environment: {
             arrowFunction: false
         }
     },
     // snapshot:{managedPaths:[]},//剔除覆盖原来的优化，从而支持监听node_modules文件的变化
     devServer: {
-        contentBase: './public',//额外静态资源路径，非webpack打包输入的文件资源
+        contentBase: './public',//额外静态资源路径，非webpack打包输出的文件资源
         publicPath: '/',
         proxy,
         hot: true,
@@ -46,7 +47,7 @@ module.exports = {
     },
     module: {
         rules: [
-            { 
+            {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 use: {
@@ -73,11 +74,11 @@ module.exports = {
                     'style-loader',
                     {
                         loader: "css-loader",
-                        options:{
-                            importLoaders:1,//在css-loader工作的过程中，如果遇到.css文件则回退给它的上一个loader进入处理流程（1代表回退一个，2代表回退俩个）
+                        options: {
+                            importLoaders: 1,//在css-loader工作的过程中，如果遇到.css文件则回退给它的上一个loader进入处理流程（1代表回退一个，2代表回退俩个）
                             esModule: false,
                         }
-                       
+
                     },
                     'postcss-loader'
                 ]
@@ -91,9 +92,9 @@ module.exports = {
                         options: {
                             // sourceMap: true,
                             modules: {
-                                auto:(resourcePath)=>{
-                                    console.log(resourcePath,'resourcePath')
-                                    if(resourcePath.includes('antd.css')){
+                                auto: (resourcePath) => {
+                                    console.log(resourcePath, 'resourcePath')
+                                    if (resourcePath.includes('antd.css')) {
                                         return false
                                     }
                                     return true
@@ -101,8 +102,8 @@ module.exports = {
                                 localIdentName: "[local]___[hash:base64:5]"
                             },
                             esModule: false,
-                            importLoaders:1//这个配置的作用为了防止less文件中使用@import引入一个css文件，如果引入的是less文件，则不需要该处理，原因在于less-loader对@import(.css|.less)处理的不同
-                            
+                            importLoaders: 1//这个配置的作用为了防止less文件中使用@import引入一个css文件，如果引入的是less文件，则不需要该处理，原因在于less-loader对@import(.css|.less)处理的不同
+
                         }
                     },
                     'postcss-loader',
@@ -123,45 +124,49 @@ module.exports = {
             //                 // outputPath:'static'
             //             }
             //     }
-                          
+
             //     ],
-                
+
             // },
             {
                 test: /\.(jpg|png)$/i,
-                type:'asset',
+                type: 'asset',
                 parser: {
                     dataUrlCondition: {
-                      maxSize: 20 * 1024 // 20kb
+                        maxSize: 20 * 1024 // 20kb
                     }
-                  },
-                  generator: {
-                    filename: 'static/[name]-[hash:8][ext]'
-                  }
-                
+                },
+                generator: {
+                    filename: 'img/[name]-[hash:8][ext]'
+                }
 
-                                 
-                
             },
             {
                 test: /\.(ttc|ttf)$/i,
-                type:'asset/resource',
-                  generator: {
-                    filename: 'static/[name]-[hash:8][ext]'
-                  }
-                
-
-                                 
-                
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name]-[hash:8][ext]'
+                }
             },
         ]
     },
 
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ template: './index.html', filename: 'index.html', chunks: ['index'] }),
-        new HtmlWebpackPlugin({ template: './aa.html', filename: 'aa.html', chunks: ['aa'] }),
-        new webpack.HotModuleReplacementPlugin()
+        new HtmlWebpackPlugin({ template: './public/index.html', filename: 'index.html', chunks: ['index'] }),
+        // new HtmlWebpackPlugin({ template: './aa.html', filename: 'aa.html', chunks: ['aa'] }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'public',
+                    globOptions: {
+                        ignore: ["**/index.html"],
+                      },
+                }
+            ]
+
+        })
     ],
 
 
